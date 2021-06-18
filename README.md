5c437af470e14d6fde6022b4c2f6f92bbb3b20cc
Mike <reinstein.mike@gmail.com>
2021-06-18 14:10:24 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,95 @@
 # collide-and-slide-2d
 slide an ellipse/sphere along a series of line segments in 2d
+
+## why?
+
+Lately I've been making some 2d platformers in javascript, and while there are practically endless resources for tile-based games, there is very little high quality material for collision detection and response between spheres/ellipsoids and 2d line segments. Allow me to qualify "high quality":
+* handles sliding along planes
+* provides robust contact details on collisions
+* **does not leak memory**
+* easy to understand
+* tiny (200 lines of code not including modules)
+* is data-oriented
+* is purely functional
+* consistent vector/matrix/line representation
+* is a pure es module
+
+So here we are.
+
+
+## usage
+
+This API may seem very odd at first glance, there are many arguments to this function.
+
+Three in particular are worth explaining a little more:
+* `lines` is an array of actual line segments. Each item in this array is 1 line. A line is simply an array with 2 `vec2` items in it (each one is an end point on the line segment)
+* `indices` is a list of indexes into the `lines` array to actually use when handling collisions. For example, if `indices` is `[ 2, 7 ]` that means `lines[2]` and `lines[7]` will actually be used when calculating the collisions.
+* `lineCount` is the number of entries to use in the `indices` array. For example, if `indices === [ 2, 7, 19, 36 ]` and `lineCount === 3` then only the first 3 items from `indices` will be used in calculating the collisions.
+
+
+**This API may seem clunky, but it's an intentional design decision because it means you can perform collisions in a tight simulation/game loop with purely preallocated memory.**
+
+
+
+```javascript
+import collideAndSlide from 'https://cdn.jsdelivr.net/gh/mreinstein/collide-and-slide-2d/collide-and-slide.js'
+import * as vec2       from 'https://cdn.jsdelivr.net/npm/gl-matrix@3/esm/vec2.js'
+
+
+const lines = [
+	// line segment 0
+	[
+		[ 0, 300], [ 100, 300 ]
+	],
+
+	// line segment 1
+	[
+		[ 100, 300 ], [ 120, 200 ]
+	]
+]
+
+// contains the index into the lines array which indicates which lines to include when colliding
+const indices = [
+	0, // refers to line segment 0
+	1  // refers to line segment 1
+]
+
+const lineCount = 2 // how many items in the indices array to include when colliding
+
+const out = vec2.create()
+
+
+const position = vec2.fromValues(50, 200) // current position of ellipsoid
+const ellipsoidRadius = [ 5, 10 ]         // the width and height radius of the ellipsoid
+const moveVel = vec2.fromValues(10, 0)    // the horizontal movement velocity is 10 right
+const gravityVel = vec2.fromValues(0, 6)  // the vertical (gravity) velocity is 6 down
+const contact = contact()                 // where the collision info is stored
+
+// returns a boolean indicating if any collision happend or not
+const collided = collideAndSlide(
+    out,
+    lines,
+    indices,
+    lineCount,
+    position,
+    ellipsoidRadius,
+    moveVel,
+    gravityVel,
+    contact
+)
+
+if (collided)
+	console.log('collision occurred. contact details:', contact)
+
+// if no collision the entity is moved the full distance specified by gravity and move velocities.
+// if collision, it will move and slide along any lines included in the lines list.
+// either way, "out" will have this updated position.
+console.log('new position:', out)
+
+```
+
+
+## credits
+
+This code is a re-implementation of Kasper Fauerby's incredible work: http://www.peroxide.dk/papers/collision/collision.pdf
+
---
+++ collide-and-slide-2d.js
@@ -0,0 +1,145 @@
+import Pool           from 'https://cdn.jsdelivr.net/gh/mreinstein/vec2-gap/pool.js'
+import * as mat2      from 'https://cdn.jsdelivr.net/npm/gl-matrix@3/esm/mat2.js'
+import * as vec2      from 'https://cdn.jsdelivr.net/npm/gl-matrix@3/esm/vec2.js'
+import segmentsSphereEllipsoid1Indexed from 'https://cdn.jsdelivr.net/gh/mreinstein/collision-2d/src/segments-ellipsoid-sweep1-indexed.js'
+import contact        from 'https://cdn.jsdelivr.net/gh/mreinstein/collision-2d/src/contact.js'
+import copyContact    from 'https://cdn.jsdelivr.net/gh/mreinstein/collision-2d/src/contact-copy.js'
+import plane          from 'https://cdn.jsdelivr.net/gh/mreinstein/collision-2d/src/plane.js'
+import sign           from 'https://cdn.jsdelivr.net/gh/mreinstein/math-gap/src/sign.js'
+import vec2SetLength  from 'https://cdn.jsdelivr.net/gh/mreinstein/vec2-gap/set-length.js'
+
+
+const VERY_CLOSE_DISTANCE = 0.005
+const MAX_RECURSION_DEPTH = 5
+
+const tmpContact = contact()
+
+
+// collision detection/resolution
+// handle collisions between a moving entity (position and radius) and static line segments
+// http://www.peroxide.dk/papers/collision/collision.pdf
+//
+// @param vec2 out resulting position is copied here
+// @param object contact the contact object to fill. if there are multiple contacts, returns last one.
+// @param array lines non-moving line segments to collide against
+// @param vec2 position the moving AABB sphere center point
+// @param vec2 ellipsoid moving AABB ellipsoid radius (x, y)
+// @param vec2 delta the displacement vector of the entity
+// @return bool true if there was a collision
+export default function collideAndSlide (
+    out,
+    lines,
+    indices,
+    lineCount,
+    position,
+    ellipsoid,
+    moveVel,
+    gravityVel,
+    contact
+) {
+    // convert position and movement velocity from R3 to ellipsoid space
+    const eSpacePosition = vec2.divide(Pool.malloc(), position, ellipsoid)
+    const eSpaceVelocity = vec2.divide(Pool.malloc(), moveVel, ellipsoid)
+
+    // reset the contact object. null means no collision
+    contact.collider = -1
+
+    // movement collision/response
+    collideWithWorld(out, contact, lines, indices, lineCount, eSpacePosition, ellipsoid, eSpaceVelocity)
+
+    // convert gravity velocity from R3 to ellipsoid space
+    vec2.divide(eSpaceVelocity, gravityVel, ellipsoid)
+
+    collideWithWorld(out, contact, lines, indices, lineCount, out, ellipsoid, eSpaceVelocity, MAX_RECURSION_DEPTH)
+
+    // Convert final result back from ellipsoid space back to R3:
+    vec2.multiply(out, out, ellipsoid)
+
+    Pool.free(eSpacePosition)
+    Pool.free(eSpaceVelocity)
+
+    return contact.collider >= 0
+}
+
+
+function collideWithWorld (out, contact, lines, indices, lineCount, pos, ellipsoid, vel, collisionRecursionDepth = 0) {
+    if (collisionRecursionDepth > MAX_RECURSION_DEPTH) {
+        vec2.copy(out, pos)
+        return
+    }
+
+    // get nearest collision from line segments
+
+    // no collision, move the full distance
+    if (!segmentsSphereEllipsoid1Indexed(lines, indices, lineCount, pos, ellipsoid, vel, tmpContact)) {
+        vec2.add(out, pos, vel)
+        return
+    }
+
+    // find the point of desired final location of the entity
+    const destinationPoint = vec2.add(Pool.malloc(), pos, vel)
+    const newBasePoint = vec2.copy(Pool.malloc(), pos)
+
+    // only update if we are not already very close and if so, we only
+    // move very close to the intersection, not the exact spot.
+    const movementDistance = vec2.length(vel) * tmpContact.time
+    if (movementDistance >= VERY_CLOSE_DISTANCE) {
+        const V = vec2SetLength(Pool.malloc(), vel, movementDistance - VERY_CLOSE_DISTANCE)
+
+        vec2.add(newBasePoint, pos, V)
+
+        // adjust line intersection point (so sliding plane will be unaffected by the fact
+        // that we move slightly less than the collision tells us)
+        vec2.normalize(V, V)
+        vec2.scale(V, V, VERY_CLOSE_DISTANCE)
+        vec2.subtract(tmpContact.position, tmpContact.position, V)
+
+        Pool.free(V)
+    }
+
+    copyContact(contact, tmpContact)
+
+    // determine the sliding plane
+
+    // project the destination point onto the sliding plane
+    const slidePlaneOrigin = tmpContact.position
+
+    const slidePlaneNormal = vec2.subtract(Pool.malloc(), newBasePoint, tmpContact.position)
+    vec2.normalize(slidePlaneNormal, slidePlaneNormal)
+
+    const slidingPlane = plane.fromPlane(plane.create(), slidePlaneOrigin, slidePlaneNormal)
+    const planeDistance = plane.signedDistanceTo(slidingPlane, destinationPoint)
+
+    const newDestinationPoint = vec2.scaleAndAdd(
+        Pool.malloc(),
+        destinationPoint,
+        slidePlaneNormal,
+        -planeDistance
+    )
+
+    // generate slide vector. Becomes the new velocity vector in next iteration
+    const newVelocityVector = vec2.subtract(Pool.malloc(), newDestinationPoint, tmpContact.position)
+
+    // dont recurse if the new velocity is very small
+    if (vec2.length(newVelocityVector) < VERY_CLOSE_DISTANCE) {
+        vec2.copy(out, newBasePoint)
+    } else {
+        collideWithWorld(
+            out,
+            contact,
+            lines,
+            indices,
+            lineCount,
+            newBasePoint,
+            ellipsoid,
+            newVelocityVector,
+            collisionRecursionDepth + 1
+        )
+    }
+
+    Pool.free(destinationPoint)
+    Pool.free(slidePlaneNormal)
+    Pool.free(newDestinationPoint)
+    Pool.free(newVelocityVector)
+    Pool.free(newBasePoint)
+}
