2f737039206727f31c65c7bdf72553ecf0ab0d06
Mike <reinstein.mike@gmail.com>
2023-05-22 11:44:17 -0700

    make unit test pass

--- test/test.js
+++ test/test.js
@@ -4,7 +4,6 @@ import collideAndSlide from '../collide-and-slide-2d.js'
 import { vec2 }        from 'https://cdn.skypack.dev/pin/gl-matrix@v3.4.3-OSmwlRYK5GW1unkuAQkN/mode=imports,min/optimized/gl-matrix.js'
 
 
-// TODO
 const lines = [
 	[ [ 265, 417 ], [ 611, 161 ] ]
 ]
@@ -26,15 +25,9 @@ const out = [0 , 0]
 
 const collision = collideAndSlide(out, lines, indices, lineCount, position, ellipsoid, moveVel, gravityVel, contact)
 
-
-console.log('collision:', collision)
-
-if (collision) {
-	console.log('contact:', contact)
-	console.log('out:', out)
-}
-
-/*
 assert.equal(collision, true)
-assert.deepEqual(contact.position, [ , ])
-*/
+assert.deepEqual(out, [ 498.9893493652344, 236.26020050048828 ])
+assert.deepEqual(contact.position, [ 125.13674926757812, 34.67463684082031 ])
+assert.deepEqual(contact.delta, [ -0, -14.283598400450344 ])
+assert.deepEqual(contact.normal, [ -0.3894166946411133, -0.9210616946220398 ])
+assert.equal(contact.time, 0.00014810243314507084)
